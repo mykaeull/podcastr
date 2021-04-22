6f2933c88ee9d7027ebe3c78855128b268552a6d
mykaeull <mykaeull99@gmail.com>
2021-04-22 00:08:19 -0300

    Criando tela de home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,53 @@
+import { GetStaticProps } from "next"
+import { api } from "../services/api"
 
-export default function Home() {
+interface Episode {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+}
+
+interface HomeProps {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
     </>
   )
 }
+
+// SSG (serve site generation)
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: { // episodes?_limit=12&_sort=published_at&_order=desc
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
+}
+
+/* SSR (server side rendering)
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    }
+  }
+}
+*/
